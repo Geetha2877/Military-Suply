f716d966dbcfa3b544013a79bc616e03d1423196
Geetha2877 <61772587+Geetha2877@users.noreply.github.com>
2020-05-26 20:10:20 +0530

    Update Box.js

--- Box.js
+++ Box.js
@@ -1,6 +1,6 @@
 
 class Box{
-    construction(x,y,width,height){
+    constructor(x,y,width,height){
         this.body=Bodies.rectangle(x,y,width,height);
         this.width=width;
         this.height=height;
